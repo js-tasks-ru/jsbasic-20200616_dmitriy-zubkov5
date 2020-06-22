cf691853ae773588a65d4d7dfacc08db64b75f40
Dima Zubkov <dimazubkov99@gmail.com>
2020-06-22 20:26:15 +0300

    done 5-task

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -5,7 +5,7 @@
  * @returns {string}
  */
 function truncate(str, maxlength) {
-  // ваш код...
+  // ваш код... 
   if (+str.length > +maxlength) {
     return str.slice(0, (maxlength - 1)) + `…`;
   } else {return str;} 
