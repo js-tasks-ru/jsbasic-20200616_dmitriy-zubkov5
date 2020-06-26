0805b6ce4dfa535649793198e0ad56d574561a35
Dima Zubkov <dimazubkov99@gmail.com>
2020-06-26 03:19:53 +0300

    done 2-module-1-task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,5 +4,19 @@
  * @returns {Number}
  */
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  let flag = 0;
+  
+  for (let key in salaries) {
+
+    if (typeof salaries[key] === "number") {
+      sum = sum + salaries[key];
+      flag++;
+    }
+  }
+  if (flag > 0) {
+    return sum;
+  } else {
+    return 0;
+  }
 }
